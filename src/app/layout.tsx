8d38d1822c5756c7b9ccf327d322d68d141736fc
John O'Neil <johnoneil.tech@gmail.com>
2026-03-25 15:13:35 -0400

    add design tokens, dark mode (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${cormorant.variable} ${dmSans.variable} h-full antialiased`}
+      className={`${cormorant.variable} ${dmSans.variable} h-full antialiased dark`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
