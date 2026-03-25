ec32f3ada62bdbe81af908fab35e6829978da9de
John <johnoneil.tech@gmail.com>
2026-03-25 15:11:49 -0400

    add design tokens, dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
-    </div>
+    <main>
+      <h1 className="font-serif text-gold text-5xl p-10">Roam</h1>
+      <p className="text-primary p-10">Design system working.</p>
+    </main>
   );
 }
