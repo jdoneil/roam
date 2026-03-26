10903e4c40be66677dd3cd490d4a246d4dfeb7a9
John <johnoneil.tech@gmail.com>
2026-03-26 14:54:02 -0400

    add nav template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <h1 className="font-serif text-gold text-5xl p-10">Roam</h1>
       <p className="text-primary p-10">Design system working.</p>
     </main>
